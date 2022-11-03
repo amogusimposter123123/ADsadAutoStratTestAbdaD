bf110b6e4bbabed96546b209f74c7816ef7c5053
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2022-11-03 11:50:58 +0200

    Update ASbdaAutoPlayerJfdDl.lua

--- ASbdaAutoPlayerJfdDl.lua
+++ ASbdaAutoPlayerJfdDl.lua
@@ -1,13 +1,29 @@
 local timer = 0
 local tab = {}
 
-function tab:PlaceTower(posx,posy,posz,tower,tim)
+function tab:PlaceTower(x,y,z, Tower, InTime, Wave)
 repeat wait() until workspace:FindFirstChild("Map")
-repeat wait() until timer >= tim
-game:GetService("Workspace").Placed:InvokeServer(Vector3.new(posx,posy,posz),1,tower,workspace.Map:FindFirstChild("Grass"))
+repeat wait() until timer >= InTime and workspace.Waves:FindFirstChild("Wave").Value == Wave
+game:GetService("Workspace").Placed:InvokeServer(Vector3.new(x,y,z),1,Tower,workspace.Map.Grass)
+end
+
+function tab:UpgradeTower(TowerNum, InTime, Wave)
+repeat wait() until workspace:FindFirstChild("Map")
+repeat wait() until timer >= InTime and workspace.Waves:FindFirstChild("Wave").Value == Wave
+game:GetService("Workspace").UpgradeTower:InvokeServer(TowerNum)
+end
+
+function tab:SellTower(TowerNum, InTime, Wave)
+repeat wait() until workspace:FindFirstChild("Map")
+repeat wait() until timer >= InTime and workspace.Waves:FindFirstChild("Wave").Value == Wave
+game:GetService("Workspace").SellTower:InvokeServer(TowerNum)
 end
 
 repeat wait() until workspace:FindFirstChild("Map")
-spawn(function() while wait(1) do timer = timer + 1 print(timer) end end)
+spawn(function() 
+  while wait(1) do 
+    timer = timer + 1 print(timer) 
+  end 
+end)
 
 return tab
