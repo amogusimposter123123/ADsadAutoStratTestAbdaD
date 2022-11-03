0ec83444679680387b4a0737ba8ef5113ff683bd
amogus <74731114+amogusimposter123123@users.noreply.github.com>
2022-11-03 11:31:07 +0200

    Update ASbdaAutoPlayerJfdDl.lua

--- ASbdaAutoPlayerJfdDl.lua
+++ ASbdaAutoPlayerJfdDl.lua
@@ -1,13 +1,13 @@
 local timer = 0
 local tab = {}
 
-function tab:PlaceTower(posx,posy,posz,tower,timee)
+function tab:PlaceTower(posx,posy,posz,tower,tim)
 repeat task.wait() until workspace:FindFirstChild("Map")
-repeat task.wait() until timer >= timee
+repeat task.wait() until timer >= tim
 game:GetService("Workspace").Placed:InvokeServer(Vector3.new(posx,posy,posz),1,tower,workspace.Map:FindFirstChild("Grass"))
 end
 
-repeat task.wait() until workspace:FindFirstChild("Map")
-while wait(1) do timer = timer + 1 print(timer) end
+repeat wait() until workspace:FindFirstChild("Map")
+spawn(function() while wait(1) do timer = timer + 1 print(timer) end end)
 
 return tab
